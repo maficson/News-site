c8e88541a62376265ecc4b49a5d8c5ed99798a32
maficson <maksimmalaruk@gmail.com>
2025-05-15 15:57:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,20 @@ from flask import Flask, request
 import os
 
 app = Flask(__name__)
+
+# Папка для збереження файлів
 UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route('/')
 def home():
-    return 'Сервер працює!'
+    return '''
+        <h1>Файл-сервер працює!</h1>
+        <form action="/upload" method="post" enctype="multipart/form-data">
+            <input type="file" name="file">
+            <input type="submit" value="Завантажити файл">
+        </form>
+    '''
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -15,8 +23,10 @@ def upload_file():
     if file:
         filepath = os.path.join(UPLOAD_FOLDER, file.filename)
         file.save(filepath)
-        return 'Файл отримано!', 200
+        return f'Файл {file.filename} отримано та збережено!'
     return 'Файл не надіслано', 400
 
+# Головний запуск з підтримкою порту Render
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
